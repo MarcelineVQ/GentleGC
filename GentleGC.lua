a41404979d9919ebb781e02ea6a493ad604ef2cc
MarcelineVQ <marcythevq@gmail.com>
2024-10-15 14:52:04 -0700

    doc

--- GentleGC.lua
+++ GentleGC.lua
@@ -1,19 +1,21 @@
 local gentleGC = CreateFrame("FRAME")
 
-local gc_count = 0
+local gc_count = 1
+
+-- ggc_debug = true
 
 gentleGC:SetScript("OnUpdate", function ()
   this.elapsed = (this.elapsed or 0) + arg1
-  if this.elapsed > 120 then
-    gc_count = gc_count + 1
-    local x = gcinfo()
+  if this.elapsed > 180 then
+    local x,d1 = gcinfo(),GetTime()
     -- print("gc "..gc_count.." "..x/1024.."mb / "..y/1024.."mb")
     collectgarbage()
-    local y = gcinfo()
+    local y,d2 = gcinfo(),GetTime()
     if ggc_debug then
-      print(format("gc %d, was %.1fmb, is %.1fmb, saved %.1fmb",gc_count,x/1024,y/1024,(x-y)/1024))
+      print(format("%d sec gc %d, was %.1fmb, is %.1fmb, saved %.1fmb, took %.2fs",180,gc_count,x/1024,y/1024,(x-y)/1024, d2 - d1))
     end
 
+    gc_count = gc_count + 1
     this.elapsed = 0
   end
 end)
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 Gentle Garbage Collection
 ---
 Automatically clear garbage without allocating more space, which causes long pauses for some players.  
+
+Garbage collection pauses are caused by having a lot of garbage to check and by needing to allocate space when there's too much garbag. This addon attemps to avoid the latter by clearing garbage before the game decides to allocate more space for it.  
+The best way to avoid the former is to turn off weighty addons you don't use often, pfquest for instance can use 100mb of addon memory while doing nothing at all.  
+
+This addon runs once ever 3 minutes, type `/run ggc_debug = 1` in game to see a report each time it runs.  
